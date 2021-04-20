ccba25fb98ec154c3574ccf78b9efbaabaf3a7f1
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-20 16:14:21 +0300

    created

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,8 +1,60 @@
-#include < iostream>
+#include <iostream>
+#include <string>
+
+// Class Hierarchy for Gas Model
+class Calculation
+{
+public:
+    virtual ~Calculation() {}
+    virtual void calculate(const std::string& file) = 0;
+};
+
+class IDEAL_GAS_Calculation : public Calculation
+{
+public:
+    void calculate(const std::string& file)
+    {
+        std::cout << "IDEAL_GAS calculation" << std::endl;
+    }
+};
+
+class LJ_GAS_Calculation : public Calculation
+{
+public:
+    void calculate(const std::string& file)
+    {
+        std::cout << "LJ_GAS calculation" << std::endl;
+    }
+};
+
+
+// Class for using
+class Calculator
+{
+public:
+    Calculator(Calculation* comp) : p(comp) {}
+    ~Calculator() { delete p; }
+    void calculate(const std::string& file)
+    {
+        p->calculate(file);
+    }
+private:
+    Calculation* p;
+};
+
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+    Calculator* p = new Calculator(new IDEAL_GAS_Calculation);
 
-	return EXIT_SUCCESS;
+    Calculator* q = new Calculator(new LJ_GAS_Calculation);
+
+
+    p->calculate("file.txt");
+
+    q->calculate("qfile.txt");
+
+    delete p;
+    delete q;
+    return 0;
 }
