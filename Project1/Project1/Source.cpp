e5f3802f4334537404e9d62476358018607fbe6f
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-24 01:20:03 +0300

    modified

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,60 +1,28 @@
 #include <iostream>
 #include <string>
 
-// Class Hierarchy for Gas Model
-class Calculation
+class A
 {
 public:
-    virtual ~Calculation() {}
-    virtual void calculate(const std::string& file) = 0;
-};
-
-class IDEAL_GAS_Calculation : public Calculation
-{
-public:
-    void calculate(const std::string& file)
-    {
-        std::cout << "IDEAL_GAS calculation" << std::endl;
-    }
-};
-
-class LJ_GAS_Calculation : public Calculation
-{
-public:
-    void calculate(const std::string& file)
-    {
-        std::cout << "LJ_GAS calculation" << std::endl;
-    }
-};
-
-
-// Class for using
-class Calculator
-{
-public:
-    Calculator(Calculation* comp) : p(comp) {}
-    ~Calculator() { delete p; }
-    void calculate(const std::string& file)
-    {
-        p->calculate(file);
-    }
+	A(int xx) :x(xx) {};
+	int get() { return x; };
+	void set(int t) { x = t; };
 private:
-    Calculation* p;
+	int x;
 };
 
+class B: public A
+{
+public:
+	B(int yy, int xx) :y(yy), A(xx) 
+	{};
+private:
+	int y;
+};
 
 int main()
 {
-    Calculator* p = new Calculator(new IDEAL_GAS_Calculation);
-
-    Calculator* q = new Calculator(new LJ_GAS_Calculation);
-
-
-    p->calculate("file.txt");
-
-    q->calculate("qfile.txt");
-
-    delete p;
-    delete q;
-    return 0;
+	B b(9, 5);
+	b.set(7);
+	std::cout << b.get() << std::endl;
 }
