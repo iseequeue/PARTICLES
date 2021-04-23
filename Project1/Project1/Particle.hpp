18fb0d33c9f6de102947118a1f08f61ca883c48d
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-24 02:46:42 +0300

    modified

--- Project1/Project1/Particle.hpp
+++ Project1/Project1/Particle.hpp
@@ -1,1 +1,102 @@
 #pragma once
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+// Class Hierarchy for particles
+class Particle
+{
+public:
+    virtual void info() = 0;
+    virtual ~Particle() {}
+
+private:
+    double m_x, m_y, m_vx, m_vy, m_ax, m_ay;
+    double m_mass, m_radius;
+    std::chrono::steady_clock::time_point m_birth;
+};
+
+class Nitrogen : public Particle
+{
+public:
+    void info()
+    {
+        std::cout << "Nitrogen" << std::endl;
+    };
+};
+
+class Oxigen : public Particle
+{
+public:
+    void info()
+    {
+        std::cout << "Oxigen" << std::endl;
+    };
+};
+
+class Nitrogen_Oxide : public Particle
+{
+public:
+    void info()
+    {
+        std::cout << "Nitrogen_Oxide" << std::endl;
+    };
+};
+
+
+// Objects Factories
+class Factory
+{
+public:
+    virtual Particle* createParticle() = 0;
+    virtual ~Factory() {}
+};
+
+class NitrogenFactory : public Factory
+{
+public:
+    Particle* createParticle()
+    {
+        return new Nitrogen;
+    }
+};
+
+class OxigenFactory : public Factory
+{
+public:
+    Particle* createParticle()
+    {
+        return new Oxigen;
+    }
+};
+
+class Nitrogen_OxideFactory : public Factory
+{
+public:
+    Particle* createParticle()
+    {
+        return new Nitrogen_Oxide;
+    }
+};
+
+
+// Создание объектов при помощи фабрик объектов
+/*
+int main()
+{
+    NitrogenFactory* n_factory = new NitrogenFactory;
+    // std::shared_ptr<NitrogenFactory> ptr1(n_factory);
+    OxigenFactory* o_factory = new OxigenFactory;
+    Nitrogen_OxideFactory* no_factory = new Nitrogen_OxideFactory;
+
+    std::vector<Particle*> v;
+    v.push_back(n_factory->createParticle());
+    v.push_back(o_factory->createParticle());
+    v.push_back(no_factory->createParticle());
+
+    for (int i = 0; i < v.size(); i++)
+        v[i]->info();
+    // ...
+}
+*/
