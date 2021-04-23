c56f39e42321b46605d6b4b521bbdf146511cd63
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-24 02:46:53 +0300

    modified

--- Project1/Project1/System.hpp
+++ Project1/Project1/System.hpp
@@ -1,1 +1,42 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <boost/asio/io_service.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+#include <memory>
+
+#include "particle.cpp";
+
+class System
+{
+public:
+	void run()
+	{
+
+	}
+
+	void ellastic_collision(std::shared_ptr<Particle> p1, std::shared_ptr<Particle> p2) {};
+
+	void unellastic_collision(std::shared_ptr<Particle> p1, std::shared_ptr<Particle> p2) {};
+
+	void calc_motion() {};
+
+	void calc_collisions() {};
+
+	void reduction_phase() {};
+
+
+private:
+	std::vector <std::shared_ptr<Particle>> particles;
+
+	const int m_width;
+	const int m_height;
+
+	boost::asio::io_service ioService;
+	boost::thread_group threadpool;
+
+	double temperature, pressure;
+	int amount;
+
+};
