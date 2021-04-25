53b62fab7189f3e2d78fa281cf75624cc356e81b
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-26 01:11:23 +0300

    modified

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
 #include <string>
 
-#include "Calculator.hpp"
-#include "Connection.hpp"
-#include "Particle.hpp"
+
 #include "System.hpp"
 
-class A
-{
-public:
-	A(int xx) :x(xx) {};
-	int get() { return x; };
-	void set(int t) { x = t; };
-private:
-	int x;
-};
-
-class B: public A
-{
-public:
-	B(int yy, int xx) :y(yy), A(xx) 
-	{};
-private:
-	int y;
-};
 
 int main()
 {
-	B b(9, 5);
-	b.set(7);
-	std::cout << b.get() << std::endl;
+
 }
